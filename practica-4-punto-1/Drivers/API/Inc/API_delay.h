54bab91b5abf7b64223ec8e2d8179c1cf334293a
Sebastian Tleye <sebastian.tleye@ookla.com>
2026-09-18 09:02:25 -0300

    separados los dos puntos, agregando punto 1

---
+++ practica-4-punto-1/Drivers/API/Inc/API_delay.h
@@ -0,0 +1,23 @@
+#ifndef API_INC_API_DELAY_H_
+#define API_INC_API_DELAY_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include "stm32f4xx_hal.h"
+
+typedef uint32_t tick_t;
+typedef bool bool_t;
+
+typedef struct {
+	tick_t startTime;
+	tick_t duration;
+	bool_t running;
+} delay_t;
+
+void delayInit(delay_t *delay, tick_t duration);
+bool_t delayRead(delay_t *delay);
+void delayWrite(delay_t *delay, tick_t duration);
+bool_t delayIsRunning(delay_t *delay);
+
+#endif /* API_INC_API_DELAY_H_ */
